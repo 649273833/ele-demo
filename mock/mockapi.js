a10dafb586f1c9e8a45b5947281e4c7dce218cfe
sorry@vip.qq.com <sorry@vip.qq.com>
2018-07-04 18:23:18 +0800

    7月4号下班更新

---
+++ mock/mockapi.js
@@ -0,0 +1,95 @@
+import Mock from 'mockjs';
+
+
+Mock.mock(/\/info.mock/,{
+  'code':1,
+  data:{
+    'data|1':[{
+      'id':1,
+      'wallte|0-999':23,
+      'redenv|0-999':21,
+      'gold|0-999':22,
+    }],
+    'address|1-10':[{
+      'id|+1':1,
+      'name':Mock.Random.cname(),
+      'gender|1':true,
+      'phone':'18381625660',
+      'region':Mock.Random.region(),
+      'city':Mock.Random.city(true),
+      'street':'时代广场B座总府路2 1802b'
+    }]
+  },
+  'message':'操作成功！',
+  'systemDate':new Date().getTime()
+})
+Mock.mock(/\/order.mock/,{
+  'code':1,
+  data:{
+    'order|5-10':[{
+      'id|+1':1,
+      'name':Mock.Random.ctitle(5,20),
+      'status|':true,
+      'time':Mock.Random.datetime('yyyy-MM-dd HH:mm:ss'),
+      'dishes':Mock.Random.cparagraph(5,20),
+      'price|1-100.1-2':1,
+      'orderimg':Mock.Random.image('250x250')
+    }]
+  },
+  'message':'获取成功！',
+  'systemDate':new Date().getTime()
+})
+
+
+Mock.mock(/\/foodlist.mock/,{
+  'code':1,
+  data:{
+    'foodlist|5-10':[{
+      'id|+1':1,
+      'name':Mock.Random.ctitle(4,8),
+      'star|1-4.1':4,
+      'sellnum|200-5000':342,
+      'specially|':true,
+      'startPrice|0-100':20,
+      'speciallyPrice|0-10':2,
+      'distance|0-10.1':2.4,
+      'time|10-120':18,
+      'sub|15-25':15,
+      'status':false,
+      'more|1-30':[{
+        'mid|+1':1,
+        'name':Mock.Random.ctitle(4)
+      }]
+    }]
+  },
+  'message':'获取成功！',
+  'systemDate':new Date().getTime()
+})
+Mock.mock(/\/searchlist.mock/,{
+  'code':1,
+  data:{
+    'searchlist|5-20':[{
+      'id|+1':1,
+      'name':Mock.Random.ctitle(1,4)
+    }]
+  },
+  'message':'获取成功！',
+  'systemDate':new Date().getTime()
+})
+Mock.mock(/\/tuijianlist.mock/,{
+  'code':1,
+  data:{
+    'tuijianlist':[
+      '烤鸭',
+      '土豆',
+      '海底捞',
+      '工作餐减25',
+      '肉',
+      '豆腐',
+      '冒菜',
+      '回锅肉',
+    ]
+  },
+  'message':'获取成功！',
+  'systemDate':new Date().getTime()
+})
